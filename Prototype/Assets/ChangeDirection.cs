9753c875dcda6be323fe5b91e2294acdaf41d8d5
ZacLindsey <zaclindsey1996@pointloma.edu>
2017-04-22 23:56:03 -0700

    prototype 1

---
+++ Prototype/Assets/ChangeDirection.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeDirection : MonoBehaviour
+{
+
+    public Vector3 newDirection1 = new Vector3();
+    public Vector3 newDirection2 = new Vector3();
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        EnemyUnit eu = other.gameObject.GetComponent<EnemyUnit>();
+        int dir = Random.Range(0, 2);
+
+        if (eu != null)
+        {
+            if (dir == 0)
+            {
+                eu.speed.x = newDirection1.x;
+                eu.speed.y = newDirection1.y;
+                eu.speed.z = newDirection1.z;
+            }
+            else if (dir == 1)
+            {
+                eu.speed.x = newDirection2.x;
+                eu.speed.y = newDirection2.y;
+                eu.speed.z = newDirection2.z;
+            }
+        }
+    }
+}
---
+++ Prototype/Assets/EnemyUnit.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyUnit : MonoBehaviour
+{
+
+    public float health;
+    public Vector3 speed = new Vector3();
+
+    // Use this for initialization
+    void Start()
+    {
+        GetComponent<Rigidbody>().velocity = transform.forward + speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.position = transform.position + speed;
+    }
+
+    public void explode()
+    {
+        Destroy(gameObject);
+    }
+
+}
